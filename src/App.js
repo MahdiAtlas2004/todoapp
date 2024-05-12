d5c8376c60f1fbb556307500f50835e18649fb05
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-12 17:08:30 +0430

    Delete button and adding operation adde.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
+import AddTodo from "./AddTodo";
 
 class App extends Component {
   state = {
     todos: [
-      { id: 1, content: "buy some mild" },
-      { id: 2, content: "play mario kart" },
+      { id: 1, content: "Start Exercise" },
+      { id: 2, content: "Go to School" },
     ],
   };
 
@@ -20,11 +21,21 @@ class App extends Component {
     });
   };
 
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      //Because the key and the value are both the same we can only write one of them.
+      todos
+    })
+  }
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -1,16 +1,18 @@
 import React from "react";
+import "./Todos.css"
 
 const Todos = ({ todos, deleteTodo }) => {
   const todosList = todos.length ? (
     todos.map((todo) => {
       return (
         <div className="collection-item" key={todo.id}>
-          <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+          <span>{todo.content}</span>
+          <button onClick={() => {deleteTodo(todo.id)}} className="delete-btn">Delete</button>
         </div>
       );
     })
   ) : (
-    <p className="center">You have no todo's left yay</p>
+    <p className="center">You have no todo's left, yay!</p>
   );
   return (
   <div className="collection todos">
